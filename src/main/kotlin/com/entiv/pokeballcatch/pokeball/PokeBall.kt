fe1b2c20d34a0aca093a459b9b45c841fff87d9b
EnTIv <38716516+EnTIv@users.noreply.github.com>
2022-07-27 14:20:29 +0800

    更新配置文件

--- src/main/kotlin/com/entiv/pokeballcatch/pokeball/PokeBall.kt
+++ src/main/kotlin/com/entiv/pokeballcatch/pokeball/PokeBall.kt
@@ -140,7 +140,7 @@ class PokeBall(
             0.0
         )
 
-        debug("玩家 ${player.name} 尝试伤害实体 ${entity}, 结果 ${damageByEntityEvent.isCancelled}")
+        debug("玩家 ${player.name} 尝试伤害实体 ${entity.name}, 结果 ${damageByEntityEvent.isCancelled}")
         if (damageByEntityEvent.isCancelled) {
             Lang.sendMessage("受到保护", player, entityPlaceholder(entity))
             return false
